3bb038b1324386e18b167440aec66223346bd63e
Danya <vladimirovichdanil@bk.ru>
2023-08-08 19:12:58 +0300

    rebuilt sorter

--- 112.py
+++ 112.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import string
+#import string
 sovpad_vkusi = pd.read_excel('data\TABAKI2.xlsx')
 black_bern = pd.read_excel('data\BlackBern.xlsx')
 
@@ -28,3 +28,26 @@ for i in range(len(words_list[:-1:])):
 
     result.append(sovpad_vkusi.loc[idx, col_name])
     print(result)
+
+if len(words_list) > 2:
+        print("!Start mathing last!")
+        last = words_list[-1]
+        
+        for i in reversed(range(len(words_list) -1)):
+            if i == len(words_list) - 2:
+                continue
+            pepa = words_list[i]
+            print("INPUT:", last, pepa)
+
+            name_of_col1 = black_bern.columns[black_bern.isin([last]).any()][0]
+            name_of_col2 = black_bern.columns[black_bern.isin([pepa]).any()][0]
+            print("OUTPUT:", name_of_col1, name_of_col2)
+
+            idx = sovpad_vkusi[sovpad_vkusi.iloc[:, 0] == name_of_col1].index[0]
+            print("IDX:", idx)   
+            col_name = sovpad_vkusi.columns[sovpad_vkusi.columns.str.contains(name_of_col2)][0] 
+            print("COLNAME", col_name)
+
+            result.append(sovpad_vkusi.loc[idx, col_name])
+
+print(result)
--- main.py
+++ main.py
@@ -25,7 +25,29 @@ def result(msg):
         print("COLNAME", col_name)
 
         result.append(sovpad_vkusi.loc[idx, col_name])
-    return result    
+
+    if len(words_list) > 2:
+        print("!Start mathing last!")
+        last = words_list[-1]
+        
+        for i in reversed(range(len(words_list) -1)):
+            if i == len(words_list) - 2:
+                continue
+            pepa = words_list[i]
+            print("INPUT:", last, pepa)
+
+            name_of_col1 = black_bern.columns[black_bern.isin([last]).any()][0]
+            name_of_col2 = black_bern.columns[black_bern.isin([pepa]).any()][0]
+            print("OUTPUT:", name_of_col1, name_of_col2)
+
+            idx = sovpad_vkusi[sovpad_vkusi.iloc[:, 0] == name_of_col1].index[0]
+            print("IDX:", idx)   
+            col_name = sovpad_vkusi.columns[sovpad_vkusi.columns.str.contains(name_of_col2)][0] 
+            print("COLNAME", col_name)
+
+            result.append(sovpad_vkusi.loc[idx, col_name])
+
+    return result
+
+print(result("Irish cream, Basillic, Haribon, Pistachio ice snow"))
            
-if __name__ == "test":
-    result("Irish cream, Basillic, Haribon, Pistachio ice snow")
