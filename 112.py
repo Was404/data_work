4ea8baa1a5ef32e58ae61558689d4bc6017f4468
Danya <vladimirovichdanil@bk.ru>
2023-08-08 16:40:03 +0300

    Sorting works correctly 2-5 names

--- 112.py
+++ 112.py
@@ -1,2 +1,30 @@
-msg = "12,12,3, 4, 5"
-print(msg.split())
+import pandas as pd
+import string
+sovpad_vkusi = pd.read_excel('data\TABAKI2.xlsx')
+black_bern = pd.read_excel('data\BlackBern.xlsx')
+
+print("Кол-во столбцов ", black_bern.shape[1])
+print("Кол-во столбцов TABAKI2", sovpad_vkusi.shape[1])
+
+msg = "Irish cream, Basillic, Haribon, Pistachio ice snow"
+#extracted_words = str.maketrans("", "", string.punctuation)
+#msg = msg.translate(extracted_words)
+words_list = msg.split(", ")
+print(words_list)
+result = []
+
+for i in range(len(words_list[:-1:])):
+    first = words_list[i]
+    second = words_list[i+1]
+    print("INPUT:", first, second)
+    name_of_col1 = black_bern.columns[black_bern.isin([first]).any()][0]
+    name_of_col2 = black_bern.columns[black_bern.isin([second]).any()][0]
+    print("OUTPUT:", name_of_col1, name_of_col2)
+
+    idx = sovpad_vkusi[sovpad_vkusi.iloc[:, 0] == name_of_col1].index[0]
+    print("IDX:", idx)   
+    col_name = sovpad_vkusi.columns[sovpad_vkusi.columns.str.contains(name_of_col2)][0] 
+    print("COLNAME", col_name)
+
+    result.append(sovpad_vkusi.loc[idx, col_name])
+    print(result)
--- main.py
+++ main.py
@@ -3,23 +3,29 @@ import pandas as pd
 sovpad_vkusi = pd.read_excel('data\TABAKI2.xlsx')
 black_bern = pd.read_excel('data\BlackBern.xlsx')
 
-print('## Сочетания вкусов табака фирмы BlackBern ##')
-print('Введите первый вкус (ВЫБОР ИЗ НАЗВАНИЙ)')
-first_obj = input()
-print('Введите второй вкус (ВЫБОР ИЗ НАЗВАНИЙ)')
-second_obj = input()
-# ЗДЕСЬ ПОЛУЧАЕМ ВКУС(НАЗВАНИЕ СТОЛБЦА) ПО ЕЁ СОДЕРЖАНИЮ
-try:
-    name_of_col1 = black_bern.columns[black_bern.isin([first_obj]).any()][0] # [0] используется, чтобы выбрать первый элемент списка (если он есть)
-    name_of_col2 = black_bern.columns[black_bern.isin([second_obj]).any()][0] 
-    print(name_of_col1)
-    print(name_of_col2)
-except:
-    print(f"\"{first_obj}\" или \"{second_obj}\" не найден в таблице")
-# найти индекс первой строки с name_of_col1 в первом столбце
-idx = sovpad_vkusi[sovpad_vkusi.iloc[:, 0] == name_of_col1].index[0]
-# найти название первого столбца, содержащего name_of_col2
-col_name = sovpad_vkusi.columns[sovpad_vkusi.columns.str.contains(name_of_col2)][0]
-result = sovpad_vkusi.loc[idx, col_name]
+def result(msg):
 
-print(f"Можно ли смешивать?\n Ответ: {result}")
+    words_list = msg.split(", ")
+    print(words_list)
+    result = []
+
+    for i in range(len(words_list[:-1:])):
+
+        first = words_list[i]
+        second = words_list[i+1]
+        print("INPUT:", first, second)
+
+        name_of_col1 = black_bern.columns[black_bern.isin([first]).any()][0]
+        name_of_col2 = black_bern.columns[black_bern.isin([second]).any()][0]
+        print("OUTPUT:", name_of_col1, name_of_col2)
+
+        idx = sovpad_vkusi[sovpad_vkusi.iloc[:, 0] == name_of_col1].index[0]
+        print("IDX:", idx)   
+        col_name = sovpad_vkusi.columns[sovpad_vkusi.columns.str.contains(name_of_col2)][0] 
+        print("COLNAME", col_name)
+
+        result.append(sovpad_vkusi.loc[idx, col_name])
+    return result    
+           
+if __name__ == "test":
+    result("Irish cream, Basillic, Haribon, Pistachio ice snow")
